64f6d17902ff0ac8c318a968edee63370946a99c
sarah.martin <sarah.martin@ENGR6F8P5S3>
2025-04-09 14:43:23 -0600

    fix

--- MobileResponsiveApp/ViewModels/GalleryViewModel.cs
+++ MobileResponsiveApp/ViewModels/GalleryViewModel.cs
@@ -8,11 +8,11 @@ public class GalleryViewModel : ObservableObject
     public ObservableCollection<GalleryItem> Items { get; set; }
     public GalleryViewModel()
     {
-        Items = new ObservableCollection<GalleryItem>(new List<GalleryItem>() 
+        Items = new ObservableCollection<GalleryItem>(new List<GalleryItem>()
         {
-            new GalleryItem("Yellow Rose", 3.99M, "rose", "Location: Ashton Gardens"),
-            new GalleryItem("Sunflower", 3.99M, "sunflower", "Location: Santaquin, UT"),
-            new GalleryItem("Lilac Bush and Moth", 4.99M, "lilac_bush", "Location: Ephraim, UT")
+            new GalleryItem("Yellow Rose", 3.99M, "https://images.pexels.com/photos/22302611/pexels-photo-22302611/free-photo-of-pink-and-yellow-rose.jpeg", "Location: Ashton Gardens"),
+            new GalleryItem("Sunflower", 3.99M, "https://images.pexels.com/photos/21430709/pexels-photo-21430709/free-photo-of-sunflower-and-white-fence.jpeg", "Location: Santaquin, UT"),
+            new GalleryItem("Lilac Bush and Moth", 4.99M, "https://images.pexels.com/photos/21575387/pexels-photo-21575387/free-photo-of-orange-moth-on-purple-flowers.jpeg", "Location: Ephraim, UT")
         });
     }
 }
