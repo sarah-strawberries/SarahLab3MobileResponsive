728941e8a800b2e017a1b2412b4a2e5e6f819c2b
sarah-strawberries <blue.howrse@gmail.com>
2025-04-09 10:17:29 -0600

    Added gitignore

---
+++ MobileResponsiveApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MobileResponsiveApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
